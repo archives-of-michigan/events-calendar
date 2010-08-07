ce049119ad2d610c450c8d7b558aa0b61c042805
Derek Kastner <dkastner@gmail.com>
2010-08-07 15:25:33 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CivilWarEvents::Application.config.secret_token = '54673fd16e818917744746fc9f3dca591e85c077301f8940e58cb8623ceac208e0352eb13f9dd5ff2b662cc578cb8d7a3e983141b50d49d803b0c599243365d7'
