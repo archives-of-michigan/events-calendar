b52d34153e461f90975d9dfe2e4908345c01d858
Derek Kastner <dkastner@gmail.com>
2010-08-12 23:25:27 -0400

    syntax fix

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -4,7 +4,10 @@ class CalendarsController < ApplicationController
   layout nil
   def show
     current_year = Date.today.year.to_i
-    @years = (current_year..(current_year + 5)).map { |y| y => y }
+    @years = (current_year..(current_year + 5)).inject({}) do |hash, y|
+      hash[y] = y
+      hash
+    end
     @months = { 'January' => 1, 'February' => 2, 'March' => 3, 'April' => 4, 'May' => 5, 'June' => 6, 
                'July' => 7, 'August' => 8, 'September' => 9, 'October' => 10, 'November' => 11, 'December' => 12 }
   end
