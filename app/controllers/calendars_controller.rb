b2e7a3ac4687613f0c997ac7c3b7657d571dffdc
Derek Kastner <dkastner@gmail.com>
2010-08-18 19:26:30 -0400

    fixed year/month dropdowns

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -4,11 +4,22 @@ class CalendarsController < ApplicationController
   layout nil
   def show
     current_year = Date.today.year.to_i
-    @years = (current_year..(current_year + 5)).inject({}) do |hash, y|
-      hash[y] = y
-      hash
+    @years = (current_year..(current_year + 5)).inject([]) do |list, y|
+      list << y
     end
-    @months = { 'January' => 1, 'February' => 2, 'March' => 3, 'April' => 4, 'May' => 5, 'June' => 6, 
-               'July' => 7, 'August' => 8, 'September' => 9, 'October' => 10, 'November' => 11, 'December' => 12 }
+    @months = [
+      [ 'January', 1 ], 
+      [ 'February', 2 ],
+      [ 'March', 3 ],
+      [ 'April', 4 ], 
+      [ 'May', 5],
+      [ 'June', 6], 
+      [ 'July', 7],
+      [ 'August', 8],
+      [ 'September', 9],
+      [ 'October', 10],
+      [ 'November', 11],
+      [ 'December', 12 ]
+    ]
   end
 end
