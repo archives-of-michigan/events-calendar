16de582d505560566cc7db71511b6cc671783bda
Derek Kastner <dkastner@gmail.com>
2010-08-21 22:25:03 -0400

    better date calc

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,7 +20,7 @@ class EventsController < ApplicationController
     day_start = DateTime.new params[:year].to_i, params[:month].to_i, params[:day].to_i, 0, 0, 0
     day_end = DateTime.new params[:year].to_i, params[:month].to_i, params[:day].to_i, 23, 59, 59
     @events = Event.find :all, 
-      :conditions => ['start <= ? AND end >= ? AND approved = ?', day_end, day_start, !user_signed_in?]
+      :conditions => ['? <= end AND ? >= start AND approved = ?', day_start, day_end, true]
     @date = Date.new params[:year].to_i, params[:month].to_i, params[:day].to_i
     respond_to do |format|
       format.html # index.html.erb
