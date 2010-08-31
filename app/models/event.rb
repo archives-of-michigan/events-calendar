00add2271866423d4c272f34f1105647360d6e7b
Derek Kastner <dkastner@gmail.com>
2010-08-31 00:18:52 -0400

    hard-coding url

--- app/models/event.rb
+++ app/models/event.rb
@@ -39,7 +39,7 @@ class Event < ActiveRecord::Base
   end
 
   def url
-    website ? website : event_path(self)
+    website ? website : "http://seekingmichigan.org/event_manager/events/#{to_param}"
   end
 
   def as_json(options = {})
