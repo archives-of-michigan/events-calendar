c27091e9a5dcfe321abeb0cfd7586c7d7ff23166
Derek Kastner <dkastner@gmail.com>
2010-08-30 21:51:49 -0400

    layout based on category

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  layout :choose_layout
+
+private
+  def choose_layout
+    if params[:category_id] == 'Civil war'
+      'civil_war'
+    else
+      'teach'
+    end
+  end
 end
