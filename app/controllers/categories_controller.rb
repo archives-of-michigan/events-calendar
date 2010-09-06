fd907d4752e5fa58d398f9fd8b34bae96b7d0a1b
Derek Kastner <dkastner@gmail.com>
2010-09-06 10:34:02 -0400

    added category event list

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,10 @@
 class CategoriesController < ApplicationController
   def index
   end
+
+  def show
+    @category = Category.find_by_name params[:id]
+    @events = @category.events.future.order 'start'
+    @events = @events.approved unless user_signed_in?
+  end
 end
