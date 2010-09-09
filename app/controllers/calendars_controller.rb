8bb439208417ecc2d127742ceea2480385c3a742
Derek Kastner <dkastner@gmail.com>
2010-09-08 22:46:56 -0400

    Teach-wide calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,8 +2,13 @@ require 'has_calendar'
 
 class CalendarsController < ApplicationController
   layout nil
+
   def show
     @category = Category.find_by_name params[:category_id]
+    @events = @category.events.future.approved
+  end
+
+  def teach
     @events = @category.events.future.approved
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ CivilWarEvents::Application.routes.draw do
   root :to => 'categories#index'
   match 'teach', :to => 'root#teach'
   match 'events/:year/:month/:day', :to => 'root#teach_events_by_day', :as => 'teach_events_by_day'
+  match 'calendars/:year/:month', :to => 'calendars#teach'
 end
